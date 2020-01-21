5ee0942b0b888cc2faa96867b7bf1a64d50e6073
Wiz <wiz_vanmeter@yahoo.com>
2020-01-20 23:56:41 -0800

    map buildings now using the hologram material

---
+++ ARSandboxV2/Assets/Scripts/hologram_size.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class hologram_size : MonoBehaviour
+{
+    public GameObject hologramObject;
+    private Material hologramMat;
+    private Slider radius_slider;
+    public float radius_size;
+    void Start()
+    {
+        radius_slider = this.gameObject.GetComponent<Slider>();
+        hologramMat = hologramObject.GetComponent<Renderer>().sharedMaterial;
+    }
+
+    public void GetRadiusSize() {
+        radius_size = radius_slider.value;
+        hologramMat.SetFloat("_circle_size", radius_size);
+    }
+}
